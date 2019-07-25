68494cb996ae4da3a8cc23f063d1b72428637528
ku <ku@Sophie-Ku-13.local>
2019-07-25 13:15:58 +0900

    merged TrieNode.java into InvertedIndex.java and deleted TrieNode.java

--- InvertedIndex.java
+++ InvertedIndex.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
 
 
 //import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
+//import javax.xml.stream.XMLInputFactory;
+//import javax.xml.stream.XMLStreamException;
 //import javax.xml.stream.events.StartElement;
 //import javax.xml.stream.events.XMLEvent;
 
@@ -199,7 +199,7 @@ public class InvertedIndex {
 	}
 
 
-	public static void main(String[] args) throws IOException, XMLStreamException {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 
 		//String file = null;
 		InvertedIndex i = new InvertedIndex();
@@ -213,91 +213,95 @@ public class InvertedIndex {
 		i.find(s);
 		//i.printTable();
 		sc.close();
-
-		//Trie t = new Trie();
+		
+		//TrieNode tn = new TrieNode();
+		Trie t = new Trie();
 		String str;
 		Set<String> keys = i.KeyWordsTable.keySet();
 		Iterator<String> itr = keys.iterator();
 		while (itr.hasNext()) {
 			str = itr.next();
-			Trie.insert(str);
-
+			t.insert(str);
 		}
 
-	}
-
-	class Trie { 
-		public Trie() {
-
-		}
-
-		// Alphabet size (# of symbols) 
-		static final int ALPHABET_SIZE = 26; 
-
-		// trie node 
-		static class TrieNode 
-		{ 
-			TrieNode[] children = new TrieNode[ALPHABET_SIZE]; 
-
-			// isEndOfWord is true if the node represents 
-			// end of a word 
-			boolean isEndOfWord; 
-
-			TrieNode(){ 
-				isEndOfWord = false; 
-				for (int i = 0; i < ALPHABET_SIZE; i++) 
-					children[i] = null; 
-			} 
-		}; 
-
-		static TrieNode root; 
-
-		// If not present, inserts key into trie 
-		// If the key is prefix of trie node, 
-		// just marks leaf node 
-		public void insert(String key) 
-		{ 
-			int level; 
-			int length = key.length(); 
-			int index; 
-
-			TrieNode pCrawl = root; 
-
-			for (level = 0; level < length; level++) 
-			{ 
-				index = key.charAt(level) - 'a'; 
-				if (pCrawl.children[index] == null) 
-					pCrawl.children[index] = new TrieNode(); 
-
-				pCrawl = pCrawl.children[index]; 
-			} 
-
-			// mark last node as leaf 
-			pCrawl.isEndOfWord = true; 
-		} 
-
-		// Returns true if key presents in trie, else false 
-		public boolean search(String key) 
-		{ 
-			int level; 
-			int length = key.length(); 
-			int index; 
-			TrieNode pCrawl = root; 
-
-			for (level = 0; level < length; level++) 
-			{ 
-				index = key.charAt(level) - 'a'; 
-
-				if (pCrawl.children[index] == null) 
-					return false; 
-
-				pCrawl = pCrawl.children[index]; 
-			} 
-
-			return (pCrawl != null && pCrawl.isEndOfWord); 
-		} 
 	}
 }
+
+class Trie { 
+	
+	public Trie() {
+
+	}
+
+	// Alphabet size (# of symbols) 
+	static final int ALPHABET_SIZE = 26; 
+
+	// trie node 
+	static class TrieNode 
+	{ 
+		TrieNode[] children = new TrieNode[ALPHABET_SIZE]; 
+
+		// isEndOfWord is true if the node represents 
+		// end of a word 
+		boolean isEndOfWord; 
+
+		TrieNode(){ 
+			isEndOfWord = false; 
+			for (int i = 0; i < ALPHABET_SIZE; i++) 
+				children[i] = null; 
+		} 
+	}; 
+
+	static TrieNode root; 
+
+	// If not present, inserts key into trie 
+	// If the key is prefix of trie node, 
+	// just marks leaf node 
+	public void insert(String key) 
+	{ 
+		int level; 
+		int length = key.length(); 
+		int index; 
+
+		TrieNode pCrawl = root; 
+
+		for (level = 0; level < length; level++) 
+		{ 
+			index = key.charAt(level) - 'a'; 
+			if (pCrawl.children[index] == null) { 
+				pCrawl.children[index] = new TrieNode();
+			}
+
+			pCrawl = pCrawl.children[index]; 
+		} 
+
+		// mark last node as leaf 
+		pCrawl.isEndOfWord = true; 
+	} 
+
+	// Returns true if key presents in trie, else false 
+	public boolean search(String key) 
+	{ 
+		int level; 
+		int length = key.length(); 
+		int index; 
+		TrieNode pCrawl = root; 
+
+		for (level = 0; level < length; level++) 
+		{ 
+			index = key.charAt(level) - 'a'; 
+
+			if (pCrawl.children[index] == null) 
+				return false; 
+
+			pCrawl = pCrawl.children[index]; 
+		} 
+
+		return (pCrawl != null && pCrawl.isEndOfWord); 
+	} 
+}
+
+
 class Pair<K, V> {
 
 	private final K element0;
